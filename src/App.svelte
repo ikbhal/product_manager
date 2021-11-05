fc033a863730d0a8916cb732a2c2f94a095c4d7c
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-05 18:57:02 +0530

    +pm

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,10 @@
 <main>
 <h1>Product Manager</h1>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/9dBnTbXgzes" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+<hr/>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/pkDXV2g_i7Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </main>
 
 <style>
